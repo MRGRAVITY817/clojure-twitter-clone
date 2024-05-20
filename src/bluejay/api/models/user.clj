c0f1b7c41eea0c1ab3e536f5280105bfd9e00e9f
Hoon Wee <mrgravity817@gmail.com>
2024-05-20 17:36:58 +0900

    feat: add web app

--- src/bluejay/api/models/user.clj
+++ src/bluejay/api/models/user.clj
@@ -1,27 +1,28 @@
 (ns bluejay.api.models.user)
 
 (def ^:private user-db-name "users")
-(def ^:private user-schema [{:db/ident       :user/email
-                             :db/valueType   :db.type/string
-                             :db/cardinality :db.cardinality/one
-                             :db/unique      :db.unique/identity}
+(def ^:private user-schema
+  [{:db/ident       :user/email
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/unique      :db.unique/identity}
 
-                            {:db/ident       :user/password
-                             :db/valueType   :db.type/string
-                             :db/cardinality :db.cardinality/one}
+   {:db/ident       :user/password
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one}
 
-                            {:db/ident       :user/username
-                             :db/valueType   :db.type/string
-                             :db/cardinality :db.cardinality/one
-                             :db/unique      :db.unique/identity}
+   {:db/ident       :user/username
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/unique      :db.unique/identity}
 
-                            {:db/ident       :user/created-at
-                             :db/valueType   :db.type/instant
-                             :db/cardinality :db.cardinality/one}
+   {:db/ident       :user/created-at
+    :db/valueType   :db.type/instant
+    :db/cardinality :db.cardinality/one}
 
-                            {:db/ident       :user/updated-at
-                             :db/valueType   :db.type/instant
-                             :db/cardinality :db.cardinality/one}])
+   {:db/ident       :user/updated-at
+    :db/valueType   :db.type/instant
+    :db/cardinality :db.cardinality/one}])
 
 (def user-db-spec {:db-name user-db-name
                    :schema  user-schema})
