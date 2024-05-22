eb53a07c804cbb33c85d5606b1a2f977f6970c25
Hoon Wee <mrgravity817@gmail.com>
2024-05-22 15:36:00 +0900

    feat: add login and logout handlers

--- src/bluejay/api/controllers/auth_controller.clj
+++ src/bluejay/api/controllers/auth_controller.clj
@@ -5,3 +5,13 @@
   (let [params (:params req)]
     (println params)
     (resp/response (str  "User created" params))))
+
+(defn login [req]
+  (let [params (:params req)]
+    (println params)
+    (resp/response (str  "User logged in" params))))
+
+(defn logout [req]
+  (let [params (:params req)]
+    (println params)
+    (resp/response (str  "User logged out" params))))
--- src/bluejay/api/handler.clj
+++ src/bluejay/api/handler.clj
@@ -16,18 +16,21 @@
    (ring/router
     [["/" {:handler #'user-ctl/default}]
      ["/auth"
-      ["/create-user" {:handler #'auth-ctl/create-user}]]])
+      ["/create-user" {:post {:handler #'auth-ctl/create-user}}]
+      ["/login"       {:post {:handler #'auth-ctl/login}}]
+      ["/logout"      {:post {:handler #'auth-ctl/logout}}]]]
+
+    {:data {:db db
+            :middleware [my-middleware
+                         parameters/parameters-middleware
+                         wrap-keyword-params]}})
 
    (ring/routes
+    (ring/redirect-trailing-slash-handler)
     (ring/create-resource-handler
      {:path "/"})
     (ring/create-default-handler
      {:not-found          (constantly {:status 404 :body "Not found"})
       :method-not-allowed (constantly {:status 405 :body "Method not allowed"})
-      :not-acceptable     (constantly {:status 406 :body "Not acceptable"})}))
-
-   {:data {:db db
-           :middleware [my-middleware
-                        parameters/parameters-middleware
-                        wrap-keyword-params]}}))
+      :not-acceptable     (constantly {:status 406 :body "Not acceptable"})}))))
 
