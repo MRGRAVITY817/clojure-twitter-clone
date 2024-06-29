4a7c7e974050fe79571b63759874f41388d53a00
Hoon Wee <mrgravity817@gmail.com>
2024-06-29 22:03:27 +0900

    feat: use classList attribute provided from solidjs

--- src/bluejay/web/pages/Events.cljs
+++ src/bluejay/web/pages/Events.cljs
@@ -10,16 +10,35 @@
     (set-pos {:x (.-clientX event)
               :y (.-clientY event)})))
 
+(def colors [:red :blue :green])
+(def color-signal (create-signal :red))
+
 (defn EventsPage []
-  (let [[pos _] position-signal]
+  (let [[pos _] position-signal
+        [color set-color] color-signal]
     #jsx
-     [:div  {:className "p-24 min-h-screen min-w-screen"
+     [:div  {:class "p-24 min-h-screen min-w-screen"
              :onMouseMove handle-mouse-move}
       [:h1 {:className "font-bold text-2xl pb-4"}
        "Mouse Position"]
-      [:div {:className "grid grid-cols-3 gap-4"}
-       [:div {:className "col-span-3"}
-        [:div {:className "bg-gray-200 p-4"}
+      [:div {:class "grid grid-cols-3 gap-4"}
+       [:div {:class "col-span-3"}
+        [:div {:class "bg-gray-200 p-4"}
          [:p "X: " (:x (pos))]
-         [:p "Y: " (:y (pos))]]]]]))
+         [:p "Y: " (:y (pos))]]]]
+
+      [:h1 {:className "font-bold text-2xl pt-4"}
+       "Class List"]
+
+      [:select {:value   (color)
+                :onInput #(set-color (-> % .-currentTarget .-value))}
+       [For {:each colors}
+        (fn [value _]
+          #jsx [:option {:value value} (str value)])]]
+
+      [:div {:class "bg-gray-200 p-4"}
+       [:p  {:classList {:text-red-500 (= (color) :red)
+                         :text-blue-500 (= (color) :blue)
+                         :text-green-500 (= (color) :green)}}
+        "Color: " (color)]]]))
 
