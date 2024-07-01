8ae747ac7f2e685d27890bac234d20d677172dac
Hoon Wee <mrgravity817@gmail.com>
2024-07-02 00:54:19 +0900

    feat: add user-event, and fix the test

--- src/bluejay/web/tests/todo_list.test.cljs
+++ src/bluejay/web/tests/todo_list.test.cljs
@@ -1,7 +1,10 @@
 (ns bluejay.web.tests.todo-list.test
   (:require ["vitest" :refer [describe expect test]]
             ["@solidjs/testing-library" :refer [render]]
+            ["@testing-library/user-event" :as user-event]
             [bluejay.web.components.TodoList :refer [TodoList]]))
+
+(def user (.setup user-event))
 
 (describe
  "<TodoList />"
@@ -13,4 +16,18 @@
             input (getByPlaceholderText "new todo here")
             button (getByText "Add Todo")]
         (-> (expect input) .toBeInTheDocument)
-        (-> (expect button) .toBeInTheDocument))))))
+        (-> (expect button) .toBeInTheDocument))))
+
+   (test
+    "adds a todo"
+    (^:async
+     fn []
+        (let [{:keys [getByPlaceholderText getByText]} (render TodoList)
+              input  (getByPlaceholderText "new todo here")
+              button (getByText "Add Todo")
+              act    (-> (.type user input "test input 1")
+                         (.then #(.click user button)))]
+          (-> act
+              (.then
+               #(do (.toHaveValue (expect input) "")
+                    (.toBeInTheDocument (expect (getByText "test input 1")))))))))))
